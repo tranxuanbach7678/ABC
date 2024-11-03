dcac757f974cda2e59f842a05bf027d928dc22ac
unknown <you@example.com>
2024-11-03 21:48:57 +0700

    Sua file in ra ten dung hon va hay hon

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 print('Hello Git')
-print('My name is Bach')
+print('My name is Tran Xuan Bach handsome')
 
 names = "Tran Xuan Bach"
 age = 18
 field = "AI"
 class_name = "TNT1A"
 
-print(names, age, fleld, class_name)
+print(names, age, field, class_name)
